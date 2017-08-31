fb3e3ecfadc1408737f53f265e5e352b4d9ec7a0
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-31 12:01:58 -0400

    fortunalteub: Remove incorrect nfc board and package options
     * Device uses sec-nfc.
    
    Change-Id: I072279be66c36bcdab50d49bd2b00c0590818d79

--- device.mk
+++ device.mk
@@ -2,7 +2,6 @@
 $(call inherit-product-if-exists, vendor/samsung/fortunalteub/fortunalteub-vendor.mk)
 
 # Inherit from common
-BOARD_NFC_CHIPSET := pn547
 $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/fortunalteub
@@ -12,8 +11,3 @@ LOCAL_PATH := device/samsung/fortunalteub
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/fortunalteub/overlay
-
-#NFC
-NXP_CHIP_TYPE := 1
-PRODUCT_PACKAGES += \
-	nfc_nci.pn54x.msm8916
