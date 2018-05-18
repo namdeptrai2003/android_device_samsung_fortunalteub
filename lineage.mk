ffe55d41284343e27d6001770d8e1647a0a1f836
Real man born on semtember <hieunam03@gmail.com>
2018-05-18 12:39:16 +0700

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
+$(call inherit-product, device/samsung/gprimelte-common/dot.mk)
 $(call inherit-product, device/samsung/fortunalteub/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fortunalteub
-PRODUCT_NAME := lineage_fortunalteub
+PRODUCT_NAME := dot_fortunalteub
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G530M
 PRODUCT_MANUFACTURER := samsung
